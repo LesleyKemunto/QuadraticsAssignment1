274935e06e36f8da76c0371610d9fcb8c3813ae8
LesleyKemunto <124598268+LesleyKemunto@users.noreply.github.com>
2023-02-08 20:08:46 +0300

    Create ENC221-0108-2022LesleyKemunto.c

---
+++ ENC221-0108-2022LesleyKemunto.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+int main (){
+float a,b,c,root1,root2;
+
+printf("%f Enter the value of a\n");
+scanf("%f",&a);
+
+printf("%f Enter the value of b \n");
+scanf("%f",&b);
+
+printf("%f Enter the value of c \n");
+scanf("%f",&c);
+
+root1 = ((-b)+(sqrt((b*b)-(4*a*c))))/(2*a);
+root2 = ((-b)-(sqrt((b*b)-(4*a*c))))/(2*a);
+
+printf("%f The 1st root is: ",root1);
+printf("  ");
+printf("  ");
+printf("%f The 2nd root is: ",root2);
+
+return 0
+}
